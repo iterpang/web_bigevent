dcff351c58a8289b42b216a1e5723be2a40b3980
iterpang <1396867215@qq.com>
2023-04-01 01:34:32 +0800

    完成了个人中心的开发

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -11,47 +11,48 @@ $(function() {
   })
 
   initUserInfo()
-
   // 初始化用户的基本信息
   function initUserInfo() {
     $.ajax({
       method: 'GET',
       url: '/my/userinfo',
       success: function(res) {
-        if (res.status !== 0) {
+        if(res.status !== 0) {
           return layer.msg('获取用户信息失败！')
         }
-        // console.log(res)
-        // 调用 form.val() 快速为表单赋值
-        form.val('formUserInfo', res.data)
+        console.log(res);
+        // 调用form.val()快速为表单赋值
+        form.val('formUserInfo', res.data) //formUserInfo指哪一个表单，lay-filter="formUserInfo"
       }
     })
   }
 
-  // 重置表单的数据
-  $('#btnReset').on('click', function(e) {
-    // 阻止表单的默认重置行为
-    e.preventDefault()
+
+  //重置表单的数据
+  $('#btnReset').on('click',function(e) {
+    e.preventDefault() //阻止表单的默认重置行为
     initUserInfo()
   })
 
-  // 监听表单的提交事件
-  $('.layui-form').on('submit', function(e) {
-    // 阻止表单的默认提交行为
-    e.preventDefault()
-    // 发起 ajax 数据请求
+
+  $('.layui-form').on('submit', function(e){
+    e.preventDefault() //阻止表单的默认提交行为
+    // 发起ajax数据请求
     $.ajax({
       method: 'POST',
       url: '/my/userinfo',
       data: $(this).serialize(),
       success: function(res) {
-        if (res.status !== 0) {
+        if (res.status !== 0){
           return layer.msg('更新用户信息失败！')
         }
         layer.msg('更新用户信息成功！')
-        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
-        window.parent.getUserInfo()
+      
+        //调用父页面中的方法，重新渲染用户的头像和用户的信息
+        window.parent.getUserInfo() //window为iframe所在窗口
       }
     })
   })
 })
+
+
